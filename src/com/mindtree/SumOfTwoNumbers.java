1b1ec02ca4a08b77a8e1906f8dadf5ca309e9f10
Harsha1305 <87223425+Harsha1305@users.noreply.github.com>
2021-07-30 15:32:21 +0530

    Update SumOfTwoNumbers.java

--- src/com/mindtree/SumOfTwoNumbers.java
+++ src/com/mindtree/SumOfTwoNumbers.java
@@ -3,9 +3,9 @@ package com.mindtree;
 public class SumOfTwoNumbers {
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		int a=10;
-		int b=20;
-		System.out.println(a+b);
+		int n1=10;
+		int n2=20;
+		System.out.println(n1+n2);
 		System.out.println("");
 	}
 
